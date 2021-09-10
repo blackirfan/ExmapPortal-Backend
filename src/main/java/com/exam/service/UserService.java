b16a7a55d45fcc302d40d46fa5b06b32fba755d6
irfan <irfanhossain2025@gmail.com>
2021-09-10 10:02:16 +0600

    Create new user service # 4

---
+++ src/main/java/com/exam/service/UserService.java
@@ -0,0 +1,14 @@
+package com.exam.service;
+
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+public interface UserService {
+
+    // creating user
+    public User createUser(User user, Set<UserRole> userRoles);
+}
